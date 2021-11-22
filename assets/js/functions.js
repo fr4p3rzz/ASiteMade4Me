63e6f94a7501cf2590916ba8aadd0415915e8ea2
fr4p3rzz <francesco95peruzzi@gmail.com>
2021-11-22 15:08:28 +0100

    major update on visual; known skills column is now an offcanvas on mobile view

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,12 +5,27 @@ let thirdCol = document.getElementById("third-col");
 window.addEventListener("resize", () => {
     if(window.innerWidth <= 1024)
     {
+        /** if we are in ipad/mobile (or res < 720p) center the content of the third column */
         thirdCol.classList.remove("col-md-8");
         thirdCol.classList.add("col-lg-12");
+
+        /** if we are in ipad/mobile (or res < 720p) view the second column as an offcanvas */
+        secondCol.setAttribute("tabindex", -1);
+        secondCol.setAttribute("aria-labelledby", "second-col-label");
+        secondCol.classList.remove("main-col");
+        secondCol.classList.add("offcanvas");
+        secondCol.classList.add("offcanvas-start");
     }
     else
     {
         thirdCol.classList.remove("col-lg-12");
         thirdCol.classList.add("col-md-8");
+
+        secondCol.classList.add("main-col");
+        secondCol.classList.remove("offcanvas");
+        secondCol.classList.remove("offcanvas-start");
+        secondCol.removeAttribute("tabindex");
+        secondCol.removeAttribute("aria-labelledby");
+        secondCol.removeAttribute("style"); //prevent visual glitches if the user has used the offcanvas mode right before switching to desktop view
     }
 });
