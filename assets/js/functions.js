ec32fbf4933ff749c3aaf7c96c65851914ae96b4
fr4p3rzz <francesco95peruzzi@gmail.com>
2021-11-22 16:05:10 +0100

    minor style fix and debug

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -23,27 +23,26 @@ window.addEventListener("resize", () => {
         thirdCol.classList.add("col-md-8");
 
         deactivateOffcanvas();
-
     }
 });
 
 
 function activateOffcanvas()
 {
-        /** if we are in ipad/mobile (or res < 720p) view the second column as an offcanvas */
-        secondCol.setAttribute("tabindex", -1);
-        secondCol.setAttribute("aria-labelledby", "second-col-label");
-        secondCol.classList.remove("main-col");
-        secondCol.classList.add("offcanvas");
-        secondCol.classList.add("offcanvas-start");
+    /** if we are in ipad/mobile (or res < 720p) view the second column as an offcanvas */
+    secondCol.setAttribute("tabindex", -1);
+    secondCol.setAttribute("aria-labelledby", "second-col-label");
+    secondCol.classList.remove("main-col");
+    secondCol.classList.add("offcanvas");
+    secondCol.classList.add("offcanvas-start");
 }
 
 function deactivateOffcanvas()
 {
-        secondCol.classList.add("main-col");
-        secondCol.classList.remove("offcanvas");
-        secondCol.classList.remove("offcanvas-start");
-        secondCol.removeAttribute("tabindex");
-        secondCol.removeAttribute("aria-labelledby");
-        secondCol.removeAttribute("style"); //prevent visual glitches if the user has used the offcanvas mode right before switching to desktop view
+    secondCol.classList.add("main-col");
+    secondCol.classList.remove("offcanvas");
+    secondCol.classList.remove("offcanvas-start");
+    secondCol.removeAttribute("tabindex");
+    secondCol.removeAttribute("aria-labelledby");
+    secondCol.removeAttribute("style"); //prevent visual glitches if the user has used the offcanvas mode right before switching to desktop view
 }
