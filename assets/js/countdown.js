588ef76e464cf856f3300e1993f8248606b292a3
fr4p3rzz <francesco95peruzzi@gmail.com>
2022-03-01 10:37:21 +0100

    optimized the countdown

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -10,18 +10,22 @@ let x = setInterval(function() {
   // Find the distance between now and the count down date
   let distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  if(distance >= 0)
+  {
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = "Heating up engines:  " + days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
+    // Display the result in the element with id="demo"
+    document.getElementById("countdown").innerHTML = "Heating up engines:  " + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  }
+  else 
+  {
+    // If the count down is finished, write some text
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "REJOYCE, MY FRIENDS";
   }
