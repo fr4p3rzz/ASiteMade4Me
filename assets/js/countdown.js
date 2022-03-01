b3e24574728c5004f1548cd5a6d2aef7ed4796fd
fr4p3rzz <francesco95peruzzi@gmail.com>
2022-03-01 08:29:55 +0100

    Elden Ring is now out: time to wait for Starfield

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("Feb 25, 2022 19:00:00").getTime();
+let countDownDate = new Date("Nov 1, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function() {
