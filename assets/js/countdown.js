1c16f88f4cea9b5f273e7d517c7ac6d9f73cffcc
fr4p3rzz <francesco95peruzzi@gmail.com>
2022-03-01 08:30:25 +0100

    data is now correct for Starfield

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("Nov 1, 2022 00:00:00").getTime();
+let countDownDate = new Date("Nov 11, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function() {
