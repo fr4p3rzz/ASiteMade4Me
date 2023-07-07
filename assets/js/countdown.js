f12f1571b455882cd87bf8587c07bffeab629ec6
Francesco Peruzzi <126003278+fr4dev@users.noreply.github.com>
2023-07-07 16:58:59 +0200

    Updated CV, starfiled release date and some data

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("Mar 31, 2023 00:00:00").getTime();
+let countDownDate = new Date("Sep 9, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function() {
